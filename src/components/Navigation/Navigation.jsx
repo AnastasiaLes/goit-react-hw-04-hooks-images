ca4f838d5d70089bdb72288deb2a18da776dc37b
Anastasia Les <anastasia@sonarmedia.com>
2022-05-09 14:31:51 +0200

    first commti

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,10 @@
+import { Nav, NavElement } from './Navigation.styled';
+
+export const Navigation = () => {
+  return (
+    <Nav>
+      <NavElement to="/">Home</NavElement>
+      <NavElement to="/movies">Movies</NavElement>
+    </Nav>
+  );
+};
